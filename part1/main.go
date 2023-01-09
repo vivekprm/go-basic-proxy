ab7cd2ef20206521b89a63fa25d4d4b12ebd416a
Vivek Mishra <v.mishra@f5.com>
2023-01-09 18:35:14 +0530

    made proxy concurrent

--- part1/main.go
+++ part1/main.go
@@ -9,33 +9,34 @@ import (
 
 func main() {
 	// 1. Listen for connections forever
-	if ln, err := net.Listen("tcp", ":8787"); err == nil {
-		for {
-			// 2. Accept connections
-			if conn, err := ln.Accept(); err == nil {
-				reader := bufio.NewReader(conn)
-				// 3. Read requests from the client.
-				if req, err := http.ReadRequest(reader); err == nil {
-					// 4. Connect to the backend web server.
-					if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
-						be_reader := bufio.NewReader(be)
-						// 5. Send the request to the backend.
-						if err := req.Write(be); err == nil {
-							// 6. Read the response from the backend.
-							if resp, err := http.ReadResponse(be_reader, req); err == nil {
-								// 7. Send the response to the client, making sure to close it.
-								resp.Close = true
-								if err := resp.Write(conn); err == nil {
-									log.Printf("proxied %s: got %d", req.URL.Path, resp.StatusCode)
-								}
-								conn.Close()
-								// Repeat back at 2: accept the next connection.
+	ln, err := net.Listen("tcp", ":8787")
+	if err == nil {
+		log.Fatalf("Failed to listen: %s", err)
+	}
+	for {
+		// 2. Accept connections
+		if conn, err := ln.Accept(); err == nil {
+			reader := bufio.NewReader(conn)
+			// 3. Read requests from the client.
+			if req, err := http.ReadRequest(reader); err == nil {
+				// 4. Connect to the backend web server.
+				if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
+					be_reader := bufio.NewReader(be)
+					// 5. Send the request to the backend.
+					if err := req.Write(be); err == nil {
+						// 6. Read the response from the backend.
+						if resp, err := http.ReadResponse(be_reader, req); err == nil {
+							// 7. Send the response to the client, making sure to close it.
+							resp.Close = true
+							if err := resp.Write(conn); err == nil {
+								log.Printf("proxied %s: got %d", req.URL.Path, resp.StatusCode)
 							}
+							conn.Close()
+							// Repeat back at 2: accept the next connection.
 						}
 					}
 				}
 			}
 		}
 	}
-
 }
